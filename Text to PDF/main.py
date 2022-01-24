518e5018945d5fd0bed0c52232d26ce32582f904
Bharadwaj Yellapragada <saisrinivasabharadwaj@gmail.com>
2022-01-24 15:12:07 +0530

    Improvments

--- Text to PDF/main.py
+++ Text to PDF/main.py
@@ -7,27 +7,29 @@ from fpdf import FPDF
 
 
 if __name__ == '__main__':
-    CONFIGURATION = [{"hfont-style":"Arial","hfont-size":15,"bfont-style":"Arial","bfont-size":11,"cell-width":200,"cell-height":10,"ln":1,'head-align':'C','text-align':'J'},
-                     {"hfont-style":"Arial","hfont-size":15,"bfont-style":"Arial","bfont-size":11,"cell-width":200,"cell-height":10,"ln":1,'head-align':'C','text-align':'J'},
-                     {"hfont-style":"Arial","hfont-size":15,"bfont-style":"Arial","bfont-size":11,"cell-width":200,"cell-height":10,"ln":1,'head-align':'C','text-align':'J'}]
+    CONFIGURATION = [{"hfont-family":"Arial","hfont-style":"B","hfont-size":15,"bfont-family":"Arial","bfont-size":11,"cell-width":200,"cell-height":10,"ln":1,'head-align':'C','text-align':'J'},
+                     {"hfont-family":"Times","hfont-style":"B","hfont-size":15,"bfont-family":"Arial","bfont-size":11,"cell-width":200,"cell-height":10,"ln":1,'head-align':'C','text-align':'J'},
+                     {"hfont-family":"Times","hfont-style":"B","hfont-size":15,"bfont-family":"Courier","bfont-size":11,"cell-width":200,"cell-height":10,"ln":1,'head-align':'C','text-align':'J'}]
 
-    heading = input("Enter the main heading: ")
+    heading = input("Enter the main title: ")
     body = input("Enter the body: ")
-    style = int(input("Choose style (1,2,3): "))
+    style = int(input("Choose style (1,2,3): "))-1
 
     pdf = FPDF()
+    pdf.set_margins(15, 5, 15)
     pdf.add_page()
 
     # set style and size of font
     # that you want in the pdf
-    pdf.set_font(CONFIGURATION[style]['hfont-style'], size=CONFIGURATION[style]['hfont-size'])
+    pdf.set_font(family=CONFIGURATION[style]['hfont-family'],style=CONFIGURATION[style]['hfont-style'], size=CONFIGURATION[style]['hfont-size'])
     pdf.cell(w=CONFIGURATION[style]['cell-width'], h=CONFIGURATION[style]['cell-height'], txt=heading, ln=CONFIGURATION[style]['ln'], align=CONFIGURATION[style]['head-align'])
-    pdf.set_font(CONFIGURATION[style]['bfont-style'], size=CONFIGURATION[style]['bfont-size'])
-    pdf.cell(w=CONFIGURATION[style]['cell-width'], h=CONFIGURATION[style]['cell-height'], txt=body, ln=CONFIGURATION[style]['ln'], align=CONFIGURATION[style]['text-align'])
+    pdf.set_font(CONFIGURATION[style]['bfont-family'], size=CONFIGURATION[style]['bfont-size'])
+    pdf.set_line_width(0)
+    pdf.multi_cell(w=CONFIGURATION[style]['cell-width'], h=CONFIGURATION[style]['cell-height'], txt=body, align=CONFIGURATION[style]['text-align'])
 
-    output_file_name = input("PDF ready! Save as: ")
-    if output_file_name[:-4] != '.pdf':
-        output_file_name += ".pdf"
+    output_file_name = heading+".pdf"
 
     # save the pdf with name .pdf
     pdf.output(output_file_name)
+
+    print("PDF ready!")
